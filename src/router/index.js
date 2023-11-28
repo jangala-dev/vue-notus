ff769b230cfa7879a9f952cd6e37b22e37d55010
cpunt <christopherpunt@hotmail.com>
2023-11-28 20:18:00 +0000

    Update paths and create router folder

---
+++ src/router/index.js
@@ -0,0 +1,68 @@
+import { createWebHistory, createRouter } from "vue-router";
+// styles
+
+// mouting point for the whole app
+
+// layouts
+import Admin from "@/layouts/Admin.vue";
+import Auth from "@/layouts/Auth.vue";
+
+// views for Admin layout
+import Dashboard from "@/views/admin/Dashboard.vue";
+import Settings from "@/views/admin/Settings.vue";
+import Tables from "@/views/admin/Tables.vue";
+import Maps from "@/views/admin/Maps.vue";
+
+// views for Auth layout
+import Login from "@/views/auth/Login.vue";
+import Register from "@/views/auth/Register.vue";
+
+// routes
+const routes = [
+  {
+    path: "/admin",
+    redirect: "/admin/dashboard",
+    component: Admin,
+    children: [
+      {
+        path: "/admin/dashboard",
+        component: Dashboard,
+      },
+      {
+        path: "/admin/settings",
+        component: Settings,
+      },
+      {
+        path: "/admin/tables",
+        component: Tables,
+      },
+      {
+        path: "/admin/maps",
+        component: Maps,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    redirect: "/auth/login",
+    component: Auth,
+    children: [
+      {
+        path: "/auth/login",
+        component: Login,
+      },
+      {
+        path: "/auth/register",
+        component: Register,
+      },
+    ],
+  },
+  { path: "/:pathMatch(.*)*", redirect: "/admin" },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
