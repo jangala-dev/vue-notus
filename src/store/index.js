8fc19050e9f100d2cc21f3fe830f3d62644f441b
cpunt <christopherpunt@hotmail.com>
2023-11-28 20:37:30 +0000

    Add login auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import router from '@/router';
 import { createStore } from "vuex";
 
 export default createStore({
@@ -15,6 +16,7 @@ export default createStore({
   actions: {
     loginUser({ commit }) {
       commit("SET_LOGGED_IN", true);
+      router.push("/admin/dashboard");
     },
     logoutUser({ commit }) {
       commit("SET_LOGGED_IN", false);
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -5,34 +5,7 @@
         <div
           class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
         >
-          <div class="rounded-t mb-0 px-6 py-6">
-            <div class="text-center mb-3">
-              <h6 class="text-blueGray-500 text-sm font-bold">
-                Sign in with
-              </h6>
-            </div>
-            <div class="btn-wrapper text-center">
-              <button
-                class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                type="button"
-              >
-                <img alt="..." class="w-5 mr-1" :src="github" />
-                Github
-              </button>
-              <button
-                class="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                type="button"
-              >
-                <img alt="..." class="w-5 mr-1" :src="google" />
-                Google
-              </button>
-            </div>
-            <hr class="mt-6 border-b-1 border-blueGray-300" />
-          </div>
-          <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
-            <div class="text-blueGray-400 text-center mb-3 font-bold">
-              <small>Or sign in with credentials</small>
-            </div>
+          <div class="flex-auto px-4 lg:px-10 py-10">
             <form>
               <div class="relative w-full mb-3">
                 <label
@@ -78,6 +51,7 @@
                 <button
                   class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                   type="button"
+                  @click="login()"
                 >
                   Sign In
                 </button>
@@ -102,15 +76,15 @@
   </div>
 </template>
 <script>
-import github from "@/assets/img/github.svg";
-import google from "@/assets/img/google.svg";
 
 export default {
   data() {
-    return {
-      github,
-      google,
-    };
+    return {};
   },
+  methods: {
+    login() {
+      this.$store.dispatch('loginUser');
+    }
+  }
 };
 </script>
